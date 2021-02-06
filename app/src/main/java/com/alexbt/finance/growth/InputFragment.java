2f8f401deca26f4ef7095d3133fa8d083811dc3d
Alex Belisle Turcot <alex.belisleturcot@gmail.com>
2021-02-06 15:30:07 -0500

    Initial

---
+++ app/src/main/java/com/alexbt/finance/growth/InputFragment.java
@@ -0,0 +1,27 @@
+package com.alexbt.finance.growth;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class InputFragment extends Fragment {
+    private View root;
+
+    // Store instance variables based on arguments passed
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    // Inflate the view for the fragment based on layout XML
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        this.root = inflater.inflate(R.layout.fragment_inputs, container, false);
+        return root;
+    }
+
+}
